3a716a8823450e5958fd3eb5e1160cc687a4a1df
Joshua Stan <lego24451@gmail.com>
2026-03-06 10:40:35 -0600

    avance ventanaEditor

---
+++ Lab_sem7_editor/src/lab_sem7_editor/VentanaEditor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab_sem7_editor;
+
+/**
+ *
+ * @author spodi
+ */
+public class VentanaEditor {
+    
+}
